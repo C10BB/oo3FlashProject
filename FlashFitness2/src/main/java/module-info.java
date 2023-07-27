a7590dde6bd030deebca6bfd49fa29b97982b30d
C10BB <113484494+C10BB@users.noreply.github.com>
2023-07-27 10:11:38 +0800

    Delete FlashFitness2 directory

--- FlashFitness2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.flashfitness {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.logging;
-
-
-    opens com.example.flashfitness to javafx.fxml;
-    exports com.example.flashfitness;
-}
